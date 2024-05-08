71a748b379857361bf145d26a353fc3af7e0ee62
Xrani <vladislavfrod@gmail.com>
2024-05-08 23:49:10 +0300

    ,,,,,

---
+++ Lesson4/scripts/task1.js
@@ -0,0 +1,24 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+let resultab;
+function calcab(a, b){
+    resultab = a * b;
+    return resultab;
+}
+let ab = calcab(10, 100);
+console.log(ab);
+
+console.log(' ');
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+let area
+    function calcr(radius){
+        area = Math.PI * Math.pow(radius, 1);
+        return area;
+    }
+    let r = 5;
+    let circle = calcr(r);
+    console.log('Радіус', r, 'Площа кола', circle);
+
+    console.log(' ');
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
